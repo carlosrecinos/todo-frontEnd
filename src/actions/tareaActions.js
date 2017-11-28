b1428a0673f4c32ba5b74800f07ab97cc89efa20
carlos recinos <carlosrecinos@gmail.com>
2017-11-28 16:40:43 -0600

    Actions terminados

--- src/actions/tareaActions.js
+++ src/actions/tareaActions.js
@@ -2,6 +2,8 @@ const ADD_TAREA = "ADD_TAREA"
 const UPDATE_TAREA = "UPDATE_TAREA"
 const DELETE_TAREA = "DELETE_TAREA"
 const FINISH_TAREA = "FINISH_TAREA"
+const SET_VISIBILITY_FILTERS = "SET_VISIBILITY_FILTERS"
+
 
 const VisibilityFilters = {
   SHOW_ALL : "SHOW_ALL",
@@ -35,5 +37,11 @@ function finishTarea(id){
   return{
     type: FINISH_TAREA,
     id
+  }
+}
+
+function setVisibilityFilters(filter){
+  return{
+    type: S
   }
 }
